ff785168025572e58d4f47ea17b68a3fc09c1ec7
leewoongi <t2korea@naver.com>
2022-03-21 02:08:58 +0900

    #4 - 클릭이벤트 및 intent 작업, 로그추가

--- app/src/main/java/com/wg/bmi/MainActivity.kt
+++ app/src/main/java/com/wg/bmi/MainActivity.kt
@@ -1,15 +1,20 @@
 package com.wg.bmi
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var tallEditText: EditText
-    lateinit var weightEditText: EditText
-    lateinit var confirmButton: Button
+    private val tag: String = "MainActivity"
+
+    private lateinit var tallEditText: EditText
+    private lateinit var weightEditText: EditText
+    private lateinit var confirmButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,11 +22,44 @@ class MainActivity : AppCompatActivity() {
 
         initialize()
 
+        confirmButton.setOnClickListener {
+            Log.d(tag, "Confirm Button click")
+
+            if(isHeightEmpty() || isWeightEmpty()){
+                Toast.makeText(this, "키와 몸무게 중 빈 값이 존재 합니다.", Toast.LENGTH_LONG).show()
+                return@setOnClickListener;
+            }
+
+            // edittext에 빈값이 올 수 없음
+
+            // 신장 입력 값 , 몸무게 입력 값 가져오기
+            val height: Int = tallEditText.text.toString().toInt()
+            val weight: Int = weightEditText.text.toString().toInt()
+            Log.d(tag, "height : $height , weight : $weight")
+
+            calculateBmi()
+        }
+
     }
 
     private fun initialize() {
         tallEditText = findViewById(R.id.et_tall)
         weightEditText = findViewById(R.id.et_weight)
         confirmButton = findViewById(R.id.bt_confirm)
+    }
+
+    private fun isHeightEmpty(): Boolean {
+        Log.d(tag, "isHeightEmpty")
+        return tallEditText.text.isEmpty()
+    }
+
+    private fun isWeightEmpty(): Boolean {
+        Log.d(tag, "isWeightEmpty")
+        return weightEditText.text.isEmpty()
+    }
+
+    private fun calculateBmi() {
+        val intent = Intent(this, ResultActivity::class.java)
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/wg/bmi/ResultActivity.kt
@@ -0,0 +1,14 @@
+package com.wg.bmi
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ResultActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+    }
+
+}
