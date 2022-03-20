b52b0be1ee1287163a96cb3a0297397666e436dc
leewoongi <t2korea@naver.com>
2022-03-20 23:10:20 +0900

    #3 -  findviewById 및 멤버변수 설정

--- app/src/main/java/com/wg/bmi/MainActivity.kt
+++ app/src/main/java/com/wg/bmi/MainActivity.kt
@@ -2,10 +2,26 @@ package com.wg.bmi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var tallEditText: EditText
+    lateinit var weightEditText: EditText
+    lateinit var confirmButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initialize()
+
+    }
+
+    private fun initialize() {
+        tallEditText = findViewById(R.id.et_tall)
+        weightEditText = findViewById(R.id.et_weight)
+        confirmButton = findViewById(R.id.bt_confirm)
     }
 }
